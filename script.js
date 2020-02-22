e1895e7f3dfb7648d66c726aeea7aeaec6a60f9b
danesn <danesnovigar@gmail.com>
2020-02-22 21:56:13 +0700

    clear the code

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-var r,g,b,a;
-
+// take the tag button into variabel
 var button = document.querySelector('.button');
-
+// add event
 button.addEventListener('click', function() {
     var angkaAcakSesuai = false
     while (angkaAcakSesuai == false) {
@@ -16,27 +15,21 @@ button.addEventListener('click', function() {
     document.body.style.backgroundColor = 'rgb('+angkaAcakSatu+','+angkaAcakDua+','+angkaAcakTiga+')';
 })
 
+// take the tag range input into variabel
 var rangeInputR = document.querySelector('#r');
 var rangeInputG = document.querySelector('#g');
 var rangeInputB = document.querySelector('#b');
 var rangeInputA = document.querySelector('#a');
-
-
+// add event
 rangeInputR.addEventListener('input', function() {
     document.body.style.backgroundColor = 'rgb('+rangeInputR.value+','+rangeInputG.value+','+rangeInputB.value+','+rangeInputA.value+'%)';
-    
 })
-
 rangeInputG.addEventListener('input', function() {
     document.body.style.backgroundColor = 'rgb('+rangeInputR.value+','+rangeInputG.value+','+rangeInputB.value+','+rangeInputA.value+'%)';
 })
-
 rangeInputB.addEventListener('input', function() {
     document.body.style.backgroundColor = 'rgb('+rangeInputR.value+','+rangeInputG.value+','+rangeInputB.value+','+rangeInputA.value+'%)';
 })
-
 rangeInputA.addEventListener('input', function() {
     document.body.style.backgroundColor = 'rgb('+rangeInputR.value+','+rangeInputG.value+','+rangeInputB.value+','+rangeInputA.value+'%)';
 })
-
-
